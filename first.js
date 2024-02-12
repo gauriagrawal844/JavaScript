341021931f882a74bfad77c17fd6d627573028ee
gauriagrawal844 <125600893+gauriagrawal844@users.noreply.github.com>
2024-02-12 21:54:13 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,24 @@
+console.log("hello");
+var myName='Gauri';
+var myAge=18;
+console.log(myName);
+console.log(typeof(myName));
+console.log( myAge); 
+console.log(typeof(myAge)); 
+var _myName='Gaurii';
+console.log(_myName);
+var _1my__Name='Gauriii';
+console.log(_1my__Name);
+//Error
+/*var 1myName='Gauri';
+console.log(1myName);*/
+var $myName='Gauriiii';
+console.log($myName);
+var i_Am_Gauri = true;
+console.log(i_Am_Gauri);
+console.log(typeof(i_Am_Gauri));
+var x;
+console.log(typeof(x));//As the datatype of x is not defined.
+
+
+
