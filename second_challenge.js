0d63161bc5dd333e4450737615cc750c882aa4e3
gauriagrawal844 <125600893+gauriagrawal844@users.noreply.github.com>
2024-02-12 21:55:43 +0530

    Create second_challenge.js

---
+++ second_challenge.js
@@ -0,0 +1,11 @@
+console.log(10+"20");
+console.log(9-"5");//bug
+console.log("Java"+"Script");
+console.log(" "+" ");
+console.log(" "+0);
+console.log("gauri"-"agrawal");
+console.log(true+false);
+console.log(true-false);
+console.log(false+true);
+console.log(false-true);
+console.log(true+true);
